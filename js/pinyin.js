dfcca0ae811deb4c70b3318dada5856ec09d5cff
Patrick Hall <pat@amundo.com>
2011-11-08 22:37:08 -0800

    depunctuation & new data

--- js/pinyin.js
+++ js/pinyin.js
@@ -6,6 +6,15 @@ var Transliterator = function(options){
     return this.rules.sort(function(a,b){ return b[0].length < a[0].length ? -1 : 1; });
   },
 
+  this.depunctuate = function(text){
+    var punc =  ' .!,?',
+        puncPAT = '([' + punc + ']+)',
+        puncRE = new RegExp(puncPAT);
+    
+    return text.split(puncRE).join('');
+   
+  },
+
   this.convert = function(text){
     this.rules = this.sortRules(this.rules);
   
@@ -14,9 +23,11 @@ var Transliterator = function(options){
           beforeRE = new RegExp(rule[0], 'g'),
           after = rule[1];
   
+   
+      text = this.depunctuate(text);
       text = text.replace(beforeRE, after);
-  
     };
+
     return text;
   }
 
@@ -28,8 +39,5 @@ var PinyinRules = [
     [ "2", "\u0301" ], 
     [ "3", "\u030C" ], 
     [ "4", "\u0300" ] 
+];
 
-  ];
-
-
- 
