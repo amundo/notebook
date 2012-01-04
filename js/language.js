41286a00891513a88cd84e69b6030d82f37c762d
Patrick Hall <pat@amundo.com>
2012-01-03 21:03:25 -0800

    Sentences->Text; Language holds language details

---
+++ js/language.js
@@ -0,0 +1,100 @@
+/*
+
+  language.js - code specific to processing a single language
+
+*/
+
+var Language = Backbone.Model.extend({
+
+  defaults :  {
+    code : 'cmn',
+    tokenPATTERN : '([ \?\.\!]+)',
+    scheme : [ ],
+  },
+
+  initialize: function(options){
+    _.bindAll(this, 'transliterate');
+    this.set(options);
+  },
+
+  transliterate : function(text){
+    _.each(this.get('scheme'), function([before, after]){
+      text = text.replace(before, after, 'g');
+    });
+
+    return text;
+  }, 
+
+  tokenize : function(text, options){
+    text = this.transliterate(text);
+    var tokenRE = new RegExp( this.get('tokenPATTERN') , 'g' );
+    var tokens = text.split(tokenRE);
+
+    return _.reject(tokens, function(e){ return e.match(/^[ ]*$/) } );
+  }
+   
+})
+
+var Languages = Backbone.Collection.extend({
+ 
+  model : Language
+
+})
+
+var kashaya = {
+
+  code : 'kju',
+
+  name : {
+    'en' : 'Kashaya Pomo'
+  },
+  tokenPATTERN : '([ \?])',
+
+  scheme : [
+      [ "^h", "\u02b0" ], 
+      [ "$", "s\u030c" ], 
+      [ "s'", "s\u0313" ], 
+      [ "7", "t\u0323" ], 
+      [ "?", "\u0294" ], 
+      [ "i'", "i\u0301" ], 
+      [ "u'", "u\u0301" ], 
+      [ "o'", "o\u0301" ], 
+      [ "e'", "e\u0301" ], 
+      [ "t\u0323'", "t\u0323\u0313" ], 
+      [ "t'", "t\u0313" ], 
+      [ "q'", "q\u0313" ], 
+      [ "c'", "c\u0313" ], 
+      [ "k'", "k\u0313" ], 
+      [ "p'", "p\u0313" ], 
+      [ "q'", "q\u0313" ], 
+      [ "a'", "a\u0301" ], 
+      [ ":", "\u00b7" ], 
+      [ ".-", ".\u0304" ], 
+      [ ".^", ".\u0302" ], 
+      [ "\u2593", "\u25fb" ], 
+      [ "O", "\u00b0" ]
+  ],
+
+};
+
+var chinese = {
+
+  code : 'cmn',
+
+  name : {
+    'en' : 'Mandarin'
+  },
+
+  tokenPATTERN : '([ \\?])',
+
+  scheme : [
+    [ "1", "\u0304" ], 
+    [ "2", "\u0301" ], 
+    [ "3", "\u030C" ], 
+    [ "4", "\u0300" ] 
+  ],
+
+};
+
+window.languages  = new Languages([kashaya, chinese]);
+
